265961ab9b92833824629411a1f9e6b9fbdb9570
Chandra Sekhar Vipparla <code.chandrasekhar@gmail.com>
2025-11-09 15:18:58 +0530

    feat: initialize project structure – Spring AOP muscle memory setup

---
+++ src/main/java/org/chandra/springaop/SpringAopMuscleMemoryApplication.java
@@ -0,0 +1,13 @@
+package org.chandra.springaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopMuscleMemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopMuscleMemoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chandra/springaop/SpringAopMuscleMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chandra.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopMuscleMemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
